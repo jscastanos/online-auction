1b94052166a502646b3a6360e6dfa8b81160dcaf
Jay Durs <joeljay20@gmail.com>
2020-01-28 11:14:33 +0800

    Add - ProductController, DB

--- OnlineAuction/Models/tblAuditTrail.cs
+++ OnlineAuction/Models/tblAuditTrail.cs
@@ -18,6 +18,7 @@ namespace OnlineAuction.Models
         public string ProductId { get; set; }
         public string CreatedBy { get; set; }
         public Nullable<System.DateTime> DateCreated { get; set; }
+        public string Remarks { get; set; }
         public Nullable<int> Status { get; set; }
     }
 }
