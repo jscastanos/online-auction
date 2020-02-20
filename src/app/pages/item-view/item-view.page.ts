84f1cf084a67bb3b78a74dd859b59ed7e6f7e949
jscastanos <jec.castanos@gmail.com>
2020-02-20 19:18:08 +0800

    Added Real-Time in bidding

--- src/app/pages/item-view/item-view.page.ts
+++ src/app/pages/item-view/item-view.page.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
-import { EnvService } from 'src/app/services/env.service';
-import { CommonService } from 'src/app/services/common.service';
-import { ProductsService } from 'src/app/services/products.service';
-import { NavController } from '@ionic/angular';
-import { get, set } from '../../services/storage.service';
-import { interval } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute, Router, NavigationEnd } from "@angular/router";
+import { EnvService } from "src/app/services/env.service";
+import { CommonService } from "src/app/services/common.service";
+import { ProductsService } from "src/app/services/products.service";
+import { NavController } from "@ionic/angular";
+import { get, set } from "../../services/storage.service";
+import { interval, Observable } from "rxjs";
+import { takeWhile } from "rxjs/operators";
 
 @Component({
-  selector: 'app-item-view',
-  templateUrl: './item-view.page.html',
-  styleUrls: ['./item-view.page.scss'],
+  selector: "app-item-view",
+  templateUrl: "./item-view.page.html",
+  styleUrls: ["./item-view.page.scss"]
 })
 export class ItemViewPage implements OnInit, OnDestroy {
-
   itemId;
   itemName;
   itemStatus;
@@ -30,6 +30,8 @@ export class ItemViewPage implements OnInit, OnDestroy {
   ratingStars;
   timer;
 
+  // REAL TIME KUNO
+  psuedoRT;
   //common
   url;
   user;
@@ -39,15 +41,18 @@ export class ItemViewPage implements OnInit, OnDestroy {
   fetchAuctionItemBiddings;
   fetchDisplayItemDetails;
 
-  constructor(private nav: NavController, private route: ActivatedRoute, private env: EnvService, private router: Router, private common: CommonService, private productService: ProductsService) {
-
-
+  constructor(
+    private nav: NavController,
+    private route: ActivatedRoute,
+    private env: EnvService,
+    private router: Router,
+    private common: CommonService,
+    private productService: ProductsService
+  ) {
     this.url = this.env.URL;
     this.user = this.common.user;
 
-
     this.route.queryParams.subscribe(params => {
-
       if (params.q != null) {
         let data = JSON.parse(params.q.toString());
         this.itemId = data.id;
@@ -58,102 +63,100 @@ export class ItemViewPage implements OnInit, OnDestroy {
         set("item", data);
 
         this.loadData();
-
       } else {
-        get("item").then((data) => {
+        get("item").then(data => {
           if (data != null) {
-            this.itemId = data["id"]
-            this.itemName = data["name"]
-            this.itemStatus = data["status"]
-
+            this.itemId = data["id"];
+            this.itemName = data["name"];
+            this.itemStatus = data["status"];
 
             this.loadData();
-
           } else {
             this.router.navigateByUrl("/category-view");
           }
-
         });
       }
-
-
     });
   }
 
   ngOnInit() {
+    this.router.events.subscribe(ev => {
+      if (ev instanceof NavigationEnd) {
+        if (ev.url.includes("/item-view")) {
+          this.psuedoRT = interval(2000).subscribe(() => {
+            this.loadData();
+          });
+        } else {
+          clearInterval(this.psuedoRT);
+          this.psuedoRT.unsubscribe();
+        }
+      }
+    });
   }
 
   async loadData() {
-    if (this.itemStatus == 1)
-      await this.getAuctionItemDetails();
-    else
-      await this.getDisplayItemDetails();
+    if (this.itemStatus == 1) await this.getAuctionItemDetails();
+    else await this.getDisplayItemDetails();
   }
 
-
   getAuctionItemDetails() {
-    this.fetchAuctionItemDetails = this.productService.getAuctionItemDetails(this.itemId).subscribe(data => {
-      this.itemDetails = data;
-      this.branchName = this.itemDetails.branchName;
-      this.categoryName = this.itemDetails.categoryName;
-      this.itemDescription = this.itemDetails.ProductDescription;
-      this.auctionID = this.itemDetails.AuctionId;
+    this.fetchAuctionItemDetails = this.productService
+      .getAuctionItemDetails(this.itemId)
+      .subscribe(data => {
+        this.itemDetails = data;
+        this.branchName = this.itemDetails.branchName;
+        this.categoryName = this.itemDetails.categoryName;
+        this.itemDescription = this.itemDetails.ProductDescription;
+        this.auctionID = this.itemDetails.AuctionId;
 
-      this.timeLimit = this.timeLeft(this.itemDetails.DateTimeLimit);
-
-      //start countdown
-      this.timer = interval(1000).subscribe(x => {
         this.timeLimit = this.timeLeft(this.itemDetails.DateTimeLimit);
+
+        //start countdown
+        this.timer = interval(1000).subscribe(x => {
+          this.timeLimit = this.timeLeft(this.itemDetails.DateTimeLimit);
+        });
+
+        this.getAuctionItemBiddings();
       });
-
-
-      this.getAuctionItemBiddings();
-    })
   }
 
   getAuctionItemBiddings() {
-    this.fetchAuctionItemBiddings = this.productService.getAuctionItemBiddings(this.auctionID, this.user.id).subscribe(data => {
-      this.biddingDetails = data;
+    this.fetchAuctionItemBiddings = this.productService
+      .getAuctionItemBiddings(this.auctionID, this.user.id)
+      .subscribe(data => {
+        this.biddingDetails = data;
 
-      //check winner
-      if (this.biddingDetails.WinnerId != null) {
+        //check winner
+        if (this.biddingDetails.WinnerId != null) {
+          let bidlist = this.biddingDetails.bidList;
 
-        let bidlist = this.biddingDetails.bidList;
-
-        for (let user in bidlist) {
-
-          if (bidlist[user]["BiddersId"] == this.biddingDetails.WinnerId) {
-            this.winner = bidlist[user]
+          for (let user in bidlist) {
+            if (bidlist[user]["BiddersId"] == this.biddingDetails.WinnerId) {
+              this.winner = bidlist[user];
+            }
           }
+        } else {
+          this.winner = null;
         }
-      } else {
-        this.winner = null;
-      }
 
-
-      this.isLoaded = true;
-    })
-
-
+        this.isLoaded = true;
+      });
   }
-
 
   bidNow() {
     let data = {
       auctionID: this.auctionID,
       name: this.itemName,
       id: this.itemId
-    }
+    };
 
     let params = {
       queryParams: {
         q: JSON.stringify(data)
       }
-    }
-    this.nav.navigateRoot(["/bid-view"], params)
+    };
+    this.nav.navigateRoot(["/bid-view"], params);
   }
-
-
 
   timeLeft(t) {
     let date = Math.floor(new Date(t).getTime() - new Date().getTime()) / 1000;
@@ -171,58 +174,53 @@ export class ItemViewPage implements OnInit, OnDestroy {
 
     seconds = Math.floor(date % 60);
 
-    return [
-      days + 'd',
-      hours + 'h',
-      minutes + 'm',
-      seconds + 's'
-    ].join(' ');
+    return [days + "d", hours + "h", minutes + "m", seconds + "s"].join(" ");
   }
 
-
-
   getDisplayItemDetails() {
-    this.fetchDisplayItemDetails = this.productService.getDisplayItemDetails(this.itemId, this.user.id).subscribe(data => {
-      this.displayDetails = data;
-      this.isLoaded = true;
-      this.branchName = this.displayDetails.branchName;
-      this.categoryName = this.displayDetails.categoryName;
-      this.itemDescription = this.displayDetails.ProductDescription;
-      this.ratingStars = this.computeRating(this.displayDetails.productRate)
-    });
+    this.fetchDisplayItemDetails = this.productService
+      .getDisplayItemDetails(this.itemId, this.user.id)
+      .subscribe(data => {
+        this.displayDetails = data;
+        this.isLoaded = true;
+        this.branchName = this.displayDetails.branchName;
+        this.categoryName = this.displayDetails.categoryName;
+        this.itemDescription = this.displayDetails.ProductDescription;
+        this.ratingStars = this.computeRating(this.displayDetails.productRate);
+      });
   }
 
   computeRating(rate) {
-    let whole, half, none, array = [];
+    let whole,
+      half,
+      none,
+      array = [];
 
     whole = Math.trunc(rate);
     half = Math.ceil(rate - whole);
     none = Math.trunc(5 - (whole + half));
 
     //construct data
-    for (const index of [].constructor(whole))
-      array.push("star")
+    for (const index of [].constructor(whole)) array.push("star");
 
-    for (const index of [].constructor(half))
-      array.push("star-half")
+    for (const index of [].constructor(half)) array.push("star-half");
 
-    for (const index of [].constructor(none))
-      array.push("star-outline")
+    for (const index of [].constructor(none)) array.push("star-outline");
 
     return array;
   }
 
   rateNow(rating) {
-    let userRate = this.productService.postRate(this.itemId, this.user.id, rating).subscribe(data => {
-      this.getDisplayItemDetails();
+    let userRate = this.productService
+      .postRate(this.itemId, this.user.id, rating)
+      .subscribe(data => {
+        this.getDisplayItemDetails();
 
-      userRate.unsubscribe();
-    })
+        userRate.unsubscribe();
+      });
   }
 
-
   unsubscription() {
-
     if (this.fetchAuctionItemDetails != null)
       this.fetchAuctionItemDetails.unsubscribe();
 
@@ -232,9 +230,7 @@ export class ItemViewPage implements OnInit, OnDestroy {
     if (this.fetchDisplayItemDetails != null)
       this.fetchDisplayItemDetails.unsubscribe();
 
-    if (this.timer != null)
-      this.timer.unsubscribe();
-
+    if (this.timer != null) this.timer.unsubscribe();
   }
   ngOnDestroy() {
     this.auctionID = null;
