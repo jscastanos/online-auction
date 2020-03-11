2fc31c917ef980e64783255963411fc7e977ea54
jscastanos <jec.castanos@gmail.com>
2020-03-11 19:10:40 +0800

    fixed merge error

--- OnlineAuction/Controllers/AccountController.cs
+++ OnlineAuction/Controllers/AccountController.cs
@@ -4,112 +4,7 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
-<<<<<<< HEAD
-using System.Linq;
 
-
-namespace OnlineAuction.Controllers
-{
-    public class AccountController : Controller
-    {
-        OnlineAuctionEntities db = new OnlineAuctionEntities();
-
-        // GET: Account
-        public ActionResult Index()
-        {
-            return RedirectToAction("Login");
-        }
-
-        public ActionResult Register()
-        {
-            return View();
-        }
-        public ActionResult Login()
-        {
-            return View();
-        }
-
-        
-
-        public ActionResult Logout()
-        {
-            Session.Abandon();
-            Session.Clear();
-            return RedirectToAction("Login");
-        }
-
-
-        public ActionResult auth(string u, string p)
-        {
-            try
-            {
-                u = Session["username"] != null ? Session["username"].ToString() : u;
-                var cu = db.tblUserManagements.SingleOrDefault(ur => ur.UserName == u && ur.Password == p);
-                var empl = db.tblEmployeesInfoes.SingleOrDefault(emp => emp.EmpId == cu.UsersId);
-
-                if (cu != null)
-                {
-                    Session["userID"] = cu.UsersId;
-                    Session["branchID"] = 1;
-                    Session["username"] = cu.UserName;
-                    Session["fullName"] = fullName(empl.FirstName, empl.MiddleName, empl.LastName);
-                    Session["Role"] = db.tblUsersRoles.SingleOrDefault(role => role.RoleId == cu.RoleId).RoleName;
-                    switch (cu.RoleId)
-                    {
-                        case "1":
-                            return RedirectToAction("Index", "Admin");
-                        case "2":
-                            return RedirectToAction("Auction", "Monitoring");
-                    }
-                    return RedirectToAction("Index", "Dashboard");
-                }
-                else
-                {
-                    Session["Error"] = "The username or password is incorrect.";
-                    return RedirectToAction("Login", "Account");
-                }
-            }
-            catch (Exception)
-            {
-                Session["Error"] = "The username or password is incorrect.";
-                return RedirectToAction("logout", "Account");
-            }
-        }
-
-        [AllowAnonymous]
-        public ActionResult RetrieveImage(string id, int type)
-        {
-            var data = db.tblBiddersInfoes.SingleOrDefault(x => x.BiddersId == id);
-            byte[] img = null;
-
-            switch (type)
-            {
-                case 1: img = data.UserImg;
-                    break;
-                case 0: img = data.CardImgFront;
-                    break;
-                case 2: img = data.CardImgBack;
-                    break;
-            }
-
-            if (img != null)
-            {
-                return File(img, "image/jpeg");
-            }
-            else
-            {
-                return File("~/nophoto.png", "image/png");
-            }
-        }
-
-        public string fullName(string a, string b, string c)
-        {
-            return a + " " + b + " " + c;
-        }
-
-        
-    }
-=======
 using System.Linq;
 
 
@@ -178,13 +73,13 @@ namespace OnlineAuction.Controllers
                         var empl = db.tblEmployeesInfoes.SingleOrDefault(emp => emp.EmpId == cu.UsersId);
                         Session["branchID"] = empl.BranchId;
 
-                        if(empl.MiddleName != null)
+                        if (empl.MiddleName != null)
                             Session["fullName"] = fullName(empl.FirstName, empl.MiddleName, empl.LastName);
                         else
                             Session["fullName"] = fullName(empl.FirstName, "", empl.LastName);
                         return RedirectToAction("Index", "Dashboard");
 
-                       
+
                     }
                 }
                 else
@@ -233,5 +128,4 @@ namespace OnlineAuction.Controllers
 
 
     }
->>>>>>> 2a29172e2df53d1d728d671143cab3dea4eea28b
 }
